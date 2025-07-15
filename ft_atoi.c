0ba4de48cbddda4298c34d43f86eb87fac6300ed
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-07-15 12:29:00 +0700

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -4,41 +4,35 @@ static int	ft_isdigit(int c)
 	return (c >= '0' && c <= '9');
 }
 
-
 int	ft_atoi(char *str)
 {
 	int	i;
 	int	res;
-	int sign;
-	int	found_num;
-	int	found_sign;
+	int	sign;
 
-	i = -1;
+	i = 0;
 	res = 0;
 	sign = 1;
-	found_num = 0;
-	found_sign = 0;
-
-	while (str[++i])
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
 	{
-		// if ((!ft_isdigit(str[i]) && !found_num) || (!ft_isdigit(str[i]) && found_num) || ((str[i] == '+' || str[i] == '-') && found_sign))
-		// 	return (0);
-		if ((str[i] == '+' || str[i] == '-'))
-		{
-			if (str[i] == '-')
-				sign = -1;
-			found_sign = 1;
-		}
-		if (ft_isdigit(str[i]))
-		{
-			found_num = 1;
-			res = (res * 10) + (str[i] - '0');
-		}
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
 	}
 	return (res * sign);
 }
 
-#include <stdio.h>
-int main(){
-	printf("%d\n", ft_atoi("-24 degree"));
-}
+// #include <stdio.h>
+// int main(){
+// 	printf("%d\n", ft_atoi("-24 degree"));
+// 	printf("%d\n", ft_atoi("--24 degree"));
+// 	printf("%d\n", ft_atoi(" +2a4 degree"));
+// 	printf("%d\n", ft_atoi("a24 degree"));
+// }
