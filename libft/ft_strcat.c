d1ef108c64ed659823f6e241ca4fe9380d530f83
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-07-26 20:38:26 +0700

    12 function left

---
+++ libft/ft_strcat.c
@@ -0,0 +1,16 @@
+
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j])
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
+}
