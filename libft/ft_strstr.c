1adb847cde80fb53b4e80c17ee5a61225cbc21e7
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-07-26 18:03:42 +0700

    strstr

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -1,5 +1,38 @@
+#include <stddef.h>
+
+int check(char *str, char *start)
+{
+	int	i;
+
+	i = 0;
+	while (start[i])
+	{
+		if ((str[i] != start[i]) || str[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 
 char *ft_strstr(char *str, char *start)
 {
-	int	i
+	if (!*start)
+		return str;
+	while (*str)
+	{
+		if (*str == *start && check(str, start))
+			return (str);
+		str++;
+	}
+	return (NULL);
 }
+
+// #include <stdio.h>
+// int main(){
+// 	char myStr[] = "The rain in Spain falls mainly on the plains";
+// char *myPtr = ft_strstr(myStr, "ain");
+// if (myPtr != NULL) {
+//   printf("%s", myPtr);
+// }
+// }
