a588a484d3cbf3a7771b93a91047398d0c080a64
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-07-26 21:49:45 +0700

    9 functions left

---
+++ libft/ft_strncpy.c
@@ -0,0 +1,19 @@
+
+
+char	*ft_strncpy(char *dst, char *src,unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	return (dst);
+}
