24fa8fa9da8229020ddf66ae5331da772a2bbf15
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-07-27 11:46:05 +0700

    Finish part 1

---
+++ libft/ft_memchr.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char *ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = (const unsigned char *)s;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return (void *)(ptr + 1);
+		i++;
+	}
+	return (NULL);
+}
---
+++ libft/ft_memmove.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char *d;
+	const unsigned char *s;
+	size_t	i;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d == s || n == 0)
+		return (dest);
+	if (d < s)
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			d[i - 1] = s[i - 1];
+			i--;
+		}
+	}
+	return (dest);
+}
