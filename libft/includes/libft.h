c3353ea0aa19ae83846815fe2fec50fc399f8db4
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-08-10 21:09:13 +0700

    Uncheck testcases

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -21,6 +21,7 @@ int		ft_tolower(int c);
 int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strstr(char *str, char *start);
 char	*ft_itoa(int n);
 char	*ft_strdup(const char *src);
 char	*ft_strchr(const char *s, int c);
