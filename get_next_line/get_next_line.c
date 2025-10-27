b4debc8cb446b622e8dbce1575f19ed5f32a8e55
Wirayabovorn007 <wirayabovorn606@gmail.com>
2025-10-27 22:11:08 +0700

    Finish gnl mandatory

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -87,3 +87,24 @@ char	*get_next_line(int fd)
 	stash = update_stash(stash);
 	return (line);
 }
+
+// #include <fcntl.h> //open()
+// #include <stdio.h> //printf()
+// #include "get_next_line.h"
+
+// int main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("test.txt", O_RDONLY); // open a test file
+// 	if (fd < 0)
+// 		return (1);
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line); //print each line
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
